b3eb13b1b737f240e0eb3fd94bba240990344628
Trương Anh Hiếu - KV Booking <hieu.ta@kiotviet.com>
2024-08-18 16:51:37 +0700

    initial-catalog-with-cqrs-vertical-slice-architecture

---
+++ src/Services/Catalog/Catalog.API/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Catalog.API.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection RegisterWebApplicationServices(this IServiceCollection services)
+    {
+        return services.RegisterMediatR()
+                       .RegisterCarter();
+    }
+
+    private static IServiceCollection RegisterMediatR(this IServiceCollection services)
+    {
+        return services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        });
+    }
+    private static IServiceCollection RegisterCarter(this IServiceCollection services)
+    {
+        return services.AddCarter(configurator: c =>
+        {
+
+        });
+    }
+
+    public static void ConfigWebApplication(this WebApplication app)
+    {
+        app.MapCarter();
+    }
+}
+
