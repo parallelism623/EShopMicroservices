7d9abf672d64b261f93b43964237636e233574a7
Trương Anh Hiếu - KV Booking <hieu.ta@kiotviet.com>
2024-09-10 18:05:14 +0700

    migration-db

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure;
-internal class DependencyInjection
+public static class DependencyInjection
 {
+    public static IServiceCollection AddServicesInfrastructure(this IServiceCollection services,  IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("Default");
+        services.AddDbContext<ApplicationDbContext>(cfg =>
+        {
+            cfg.UseSqlServer(connectionString, builder =>
+            {
+                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+            });
+        });
+
+        return services;
+    }
 }
