5d294e08cf4356d7259af0d281a8313e6d1aa66e
parallelism <hieudev623@gmail.com>
2024-10-23 00:21:43 +0700

    feat: auto migration in dev env

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,12 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 var configuration = builder.Configuration;
 builder.Services.AddServicesInfrastructure(configuration);
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
+if(app.Environment.IsDevelopment())
+{
+    Log.Information("initial database");
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.MigrateAsync().GetAwaiter().GetResult();
+}
 
 app.Run();
 
--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Ordering.Infrastructure.Data.Extensions;
 public static class DatabaseExtensions
 {
-
+    
 }
